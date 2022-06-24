49d89674acd96ca2dd3b944ed28265ac00156c94
mskim58 <mskim58@crscube.io>
2022-06-24 16:49:09 +0900

    emitType returnValue nullable로 수정

--- src/components/dialog.vue
+++ src/components/dialog.vue
@@ -1,103 +1,98 @@
 <template>
-<dialog
-:class="rootClasses"
-:role="roleAttr"
-ref="Dialog"
-@click.self="close"
-@close="emits('close',Dialog.returnValue)"
-@cancel="emits('cancel',Dialog.returnValue)"
-:aria-modal="modal"
-:aria-labelledby="titleRefId"
-:aria-label="label"
-:aria-describedby="contentId"
->
-<div :class="containerClasses">
-<template v-if="alert">
-<slot></slot>
-</template>
-<form method="dialog"
-:class="formClasses" v-else>
-<slot></slot>
-</form>
-</div>
-</dialog>
+  <dialog
+    :class="rootClasses"
+    :role="roleAttr"
+    ref="Dialog"
+    @click.self="close"
+    @close="emits('close', Dialog.returnValue)"
+    @cancel="emits('cancel', Dialog.returnValue)"
+    :aria-modal="modal"
+    :aria-labelledby="titleRefId"
+    :aria-label="label"
+    :aria-describedby="contentId"
+  >
+    <div :class="containerClasses">
+      <template v-if="alert">
+        <slot></slot>
+      </template>
+      <form method="dialog" :class="formClasses" v-else>
+        <slot></slot>
+      </form>
+    </div>
+  </dialog>
 </template>
 <script setup lang="ts">
 interface propTypes {
-    label?: string,
-    containerClass?: string,
-    formClass?: string,
-    classNames?: object | Function,
-    rootClass?: string,
-    alert?: boolean,
-    modal?: boolean,
-    titleId?: string,
-    contentId?: string,
+  label?: string;
+  containerClass?: string;
+  formClass?: string;
+  classNames?: object | Function;
+  rootClass?: string;
+  alert?: boolean;
+  modal?: boolean;
+  titleId?: string;
+  contentId?: string;
 }
 interface emitTypes {
-    (event: 'cancel', returnValue: string): void,
-    (event: 'close', returnValue: string): void
+  (event: "cancel", returnValue?: string): void;
+  (event: "close", returnValue?: string): void;
 }
 const {
-    alert = false,
-    modal = false,
-    classNames = ()=>{
-        return {
-        container: 'dialog-container',
-        root: 'dialog-root',
-        form: 'dialog-form'
-        }
-    },
-    containerClass,
-    rootClass,
-    formClass,
-    label,
-    titleId,
-    contentId
-} = defineProps<propTypes>()
-const emits = defineEmits<emitTypes>()
-const Dialog = $ref({})
-const close = (arg)=>{
-    Dialog.close(arg)
-}
-const open = ()=>{
-    if (modal) Dialog.showModal()
-    else Dialog.show()
-}
+  alert = false,
+  modal = false,
+  classNames = () => {
+    return {
+      container: "dialog-container",
+      root: "dialog-root",
+      form: "dialog-form",
+    };
+  },
+  containerClass,
+  rootClass,
+  formClass,
+  label,
+  titleId,
+  contentId,
+} = defineProps<propTypes>();
+const emits = defineEmits<emitTypes>();
+const Dialog = $ref({});
+const close = (arg) => {
+  Dialog.close(arg);
+};
+const open = () => {
+  if (modal) Dialog.showModal();
+  else Dialog.show();
+};
 const headingSelector: Array<String> = [
-    'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
-    "[role='heading']"
-]
-const titleRefId = $computed(()=>{
-    if (label||titleId) return label||titleId
-    else {
-        const title = Dialog?.querySelector(headingSelector.join(','))
-        return title?.id
-    }
-})
-const roleAttr = $computed(()=>{
-    return alert ? 'dialog' : 'alertdialog' 
-})
-const containerClasses = $computed(()=>{
-    return [
-        classNames().container,
-        containerClass ?? null
-    ]
-})
-const rootClasses = $computed(()=>{
-    return [
-        classNames().root,
-        rootClass ?? null
-    ]
-})
-const formClasses = $computed(()=>{
-    return [
-        classNames().form,
-        formClass ?? null
-    ]
-})
+  "h1",
+  "h2",
+  "h3",
+  "h4",
+  "h5",
+  "h6",
+  "[role='heading']",
+];
+const titleRefId = $computed(() => {
+  if (label || titleId) return label || titleId;
+  else {
+    const title = Dialog?.querySelector(headingSelector.join(","));
+    return title?.id;
+  }
+});
+const roleAttr = $computed(() => {
+  return alert ? "dialog" : "alertdialog";
+});
+const containerClasses = $computed(() => {
+  return [classNames().container, containerClass ?? null];
+});
+const rootClasses = $computed(() => {
+  return [classNames().root, rootClass ?? null];
+});
+const formClasses = $computed(() => {
+  return [classNames().form, formClass ?? null];
+});
 defineExpose({
-    close,
-    open 
-})
+  close,
+  open,
+});
 </script>
